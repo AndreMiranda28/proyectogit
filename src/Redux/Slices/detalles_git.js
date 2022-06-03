53e117ac138161a0f307c4ef71a75b0e975c2f5c
AndreMiranda28 <andre98530@gmail.com>
2022-06-02 22:45:18 -0500

    Modificando la estructura -proyecto commits git

--- src/Redux/Slices/detalles_git.js
+++ src/Redux/Slices/detalles_git.js
@@ -2,75 +2,21 @@
 import { createSlice,createAsyncThunk } from '@reduxjs/toolkit'
 import Axios from "axios";
 
-//USANDO LA METODOLOGÍA ACTUALIZADA DE REDUX 2022
-//ESTADO INICIAL
 const initialState = {    
-    pokemon:300,
-    cuenta:0,
     loading:false,
-    pokemonLista:[],
-    pokemonHabilidades:0,
-    montoDeposito:0,
+    listcommits:[],
     error:'' 
 }; 
 
-export const getPokemon=createAsyncThunk(
-    "operaciones/getPokemon",
+//ASINCRONO
+export const getCommints=createAsyncThunk(
+    "operaciones/getCommints",
     async ({nombre},{dispatch,state}) => {
-        //console.log(dispatch)
-        //console.log(`https://pokeapi.co/api/v2/pokemon/${nombre}`)
-       const response=await  Axios.get(`https://pokeapi.co/api/v2/pokemon/${nombre}`)
-       dispatch(getPokemonHabilidades(2))
 
-       return [response.data] //porque es un array por eso le pongo []
-       // return fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`).then((res)=>
-         //  res.json()
-       // )
-    }
-)
-export const recibodata=(valor,data)=>{
-    var data1=data;
+       const response=await  Axios.get("https://api.github.com/users/AndreMiranda28/repos")
+       //console.log(response.data);
+       return response.data 
 
-    createAsyncThunk(
-        
-    )
-
-
-}
-export const handleChangeMontoApertura=createAsyncThunk(
-    "operaciones/handleChangeMontoApertura",
-    async (payload,{dispatch,getState}) => {   
-      //console.log(getState)
-        //console.log(`https://pokeapi.co/api/v2/pokemon/${nombre}`)
-       //const response=await  Axios.get(`https://pokeapi.co/api/v2/pokemon/${nombre}`)
-       dispatch(comprarPokemon(3));
-       dispatch(regresarPokemon(1));
-
-       //dispatch(obtenerdata(state.pokemon))
-       //console.log(getState);    
-       //let data=initialState
-      // data={...data,error:"jiji"};
-       //console.log(initialState.pokemon);
-       // console.log("Estas dentro del handle")
-       return payload//porque es un array por eso le pongo []
-       // return fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`).then((res)=>
-         //  res.json()
-       // )
-    }
-)
-export const obtenerdata=createAsyncThunk(
-    "operaciones/obtenerdata",
-    async (payload,{dispatch,getState}) => {   
-      //console.log(getState)
-        //console.log(`https://pokeapi.co/api/v2/pokemon/${nombre}`)
-       //const response=await  Axios.get(`https://pokeapi.co/api/v2/pokemon/${nombre}`)
-       //console.log(payload);    
-       console.log(payload);
-       // console.log("Estas dentro del handle")
-       return payload//porque es un array por eso le pongo []
-       // return fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`).then((res)=>
-         //  res.json()
-       // )
     }
 )
 
@@ -79,45 +25,26 @@ export const operacionesSlice = createSlice({
     name: 'operaciones',
     initialState,
     reducers: {
-        comprarPokemon: (state,action) => {
-        state.pokemon += action.payload
-        console.log(state.pokemon)
-        //state.cuenta+=1
-        },
-        regresarPokemon: (state) => {
-        state.pokemon -= 1
-        },
-        getPokemonHabilidades: (state,action) => {
-        state.pokemonHabilidades += action.payload
-        }
-        
+
     },
     extraReducers: {
-        [getPokemon.pending]:(state,action) => {
+        [getCommints.pending]:(state,action) => {
             state.loading=true
 
         },
-        [getPokemon.fulfilled]:(state,action) => { 
-            state.pokemonLista=action.payload,
+        [getCommints.fulfilled]:(state,action) => { 
+            state.listcommits=action.payload,
             state.loading=false
         },
-        [getPokemon.rejected]:(state,action) => {
+        [getCommints.rejected]:(state,action) => {
             state.loading=false
-            state.error="No se pudo cargar la data"
-            state.pokemonLista=[]
+            state.error="Try again, please"
+            state.listcommits=[]
 
-        },
-        [handleChangeMontoApertura.pending]:(state,action) => {
-            state.loading=true
-
-        },
-        [handleChangeMontoApertura.fulfilled]:(state,action) => { 
-            //state.pokemon=action.payload,
-            state.loading=false
         },
 
     }
   })
 
-  export const  {comprarPokemon,regresarPokemon,getPokemonHabilidades} =operacionesSlice.actions;
+  export const  {} =operacionesSlice.actions;
   export default operacionesSlice.reducer
